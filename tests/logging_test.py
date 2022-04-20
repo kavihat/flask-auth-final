b3be399d1e9cb5eb0dd5fd9cf6e115b91ccabd17
Kavitha <kavita.kannanunny@gmail.com>
2022-04-19 21:22:33 -0400

    Initial commit with logging tests

---
+++ tests/logging_test.py
@@ -0,0 +1,14 @@
+"""This test the logging"""
+import os
+
+from click.testing import CliRunner
+runner = CliRunner()
+
+
+def test_create_log_folder():
+    """This test the logging"""
+    root = os.path.dirname(os.path.abspath(__file__))
+    logdir = os.path.join(root, '../app/logs')
+    assert os.path.exists(logdir) is True
+
+
