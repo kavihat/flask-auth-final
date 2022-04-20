f0eb4b4530c61b6bbeff472a5ef2d992682eaad7
Kavitha <kavita.kannanunny@gmail.com>
2022-04-19 23:44:20 -0400

    added more testcases

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -2,11 +2,23 @@
 import os
 
 from click.testing import CliRunner
+
+from app import create_log_folder
+
 runner = CliRunner()
 
 
-def test_create_log_folder():
+def test_create_response_folder():
     """This test the logging"""
+    response = runner.invoke(create_log_folder)
+    assert response.exit_code == 0
+
+def test_create_request_log():
+    request = runner.invoke(create_log_folder)
+    assert request.exit_code == 0
+
+def test_path_exist():
     root = os.path.dirname(os.path.abspath(__file__))
     logdir = os.path.join(root, '../app/logs')
     assert os.path.exists(logdir) is True
+
