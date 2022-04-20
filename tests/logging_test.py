266db264f768ee0a422927c93da5f7a2b95c620d
Kavitha <kavita.kannanunny@gmail.com>
2022-04-20 01:01:35 -0400

    New logging test cases

--- tests/logging_test.py
+++ tests/logging_test.py
@@ -1,24 +1,17 @@
 """This test the logging"""
 import os
 
-from click.testing import CliRunner
 
-from app import create_log_folder
-
-runner = CliRunner()
-
-
-def test_create_response_folder():
-    """This test the logging"""
-    response = runner.invoke(create_log_folder)
-    assert response.exit_code == 0
-
-def test_create_request_log():
-    request = runner.invoke(create_log_folder)
-    assert request.exit_code == 0
-
-def test_path_exist():
+def is_path_exist(logfile):
     root = os.path.dirname(os.path.abspath(__file__))
-    logdir = os.path.join(root, '../app/logs')
-    assert os.path.exists(logdir) is True
+    logfile = os.path.join(root, '../app/logs/'+logfile)
+    assert os.path.exists(logfile) is True
+
+
+def test_log_files_exists(client):
+    client.get("/")
+    is_path_exist('app.log')
+    is_path_exist('request.log')
+
+
 
